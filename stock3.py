8675b3dbd482d13d27f3a13d6795b747f2454aec
nmd1 <ndureus@nyu.edu>
2021-05-09 18:33:07 -0400

    rough completion of non-sentiment model

--- stock3.py
+++ stock3.py
@@ -91,14 +91,148 @@ def processMultiData(many_data, panda=True,  look_fwd=1):
     
     return d 
     
+def processMultiDataSimple(many_data, panda=True):
+    if(len(many_data) == 0): return None
+    #print("metadata",data["meta"].keys())
+
+    total_index = 0
+    total_array = []
+    only_one_header_pass = True
+    for data in many_data:
+        if("close" not in data["indicators"]["quote"][0].keys()):
+            print("one of the stocks doesn't have a close parameter")
+            continue
+        
+        prices = data["indicators"]["quote"][0]["close"]
+        
+        times_list_next = []
+
+        if panda and only_one_header_pass:
+            times_list_next.append(np.array(["index", "price","direction"]))
+            only_one_header_pass = False
+            
+            
+        for i in range(len(prices)-1):
+            array_lst = []
+            if panda: array_lst.append(total_index)
+            array_lst.append(prices[i])
+            
+            
+            if(prices[i+1] != None and prices[i] != None):
+                delta = prices[i+1]-prices[i]
+                if(delta != 0):
+                    norm = delta / abs(delta)
+                else:
+                    norm = 0
+                array_lst.append(norm)
+            else:
+                array_lst.append(None)
+            times_list_next.append(np.array(array_lst))
+            total_index+=1
+
+        total_array += times_list_next
+    d = np.array(total_array)
+    
+    # print(d[0])
+    # print(d[1])
+    # print(d[2])
+
+    #print(d[1:,-1:])
+    
+    #print(d[1:,1:-1])
+    if panda:
+        return (pd.DataFrame(data=d[1:,1:-1],
+                    index=d[1:,0],
+                    columns=d[0,1:-1]),
+                pd.DataFrame(data=d[1:,-1:],
+                    index=d[1:,0],
+                    columns=d[0,-1:])
+        )
+                
+    
+    return d 
 
 
-def getAndProcessMultiFinancialDataJSON(stocks, interval="2m", rge="1d", panda=True, look_fwd=1):
+def processMultiDataSentiment(many_data, sentiment, panda=True):
+    if(len(many_data) == 0): return None
+    #print("metadata",data["meta"].keys())
+
+    total_index = 0
+    total_array = []
+    only_one_header_pass = True
+    for stocK_index,data in enumerate(many_data):
+        if("close" not in data["indicators"]["quote"][0].keys()):
+            print("one of the stocks doesn't have a close parameter")
+            continue
+        
+        prices = data["indicators"]["quote"][0]["close"]
+        
+        times_list_next = []
+
+        if panda and only_one_header_pass:
+            times_list_next.append(np.array(["index", "price","sentiment","direction"]))
+            only_one_header_pass = False
+            
+            
+        for i in range(len(prices)-1):
+            array_lst = []
+            if panda: array_lst.append(total_index)
+            array_lst.append(prices[i])
+            array_lst.append(sentiment[stocK_index])
+
+            sentiment
+            if(prices[i+1] != None and prices[i] != None):
+                delta = prices[i+1]-prices[i]
+                if(delta != 0):
+                    norm = delta / abs(delta)
+                else:
+                    norm = 0
+                array_lst.append(norm)
+            else:
+                array_lst.append(None)
+            times_list_next.append(np.array(array_lst))
+            total_index+=1
+
+        total_array += times_list_next
+    d = np.array(total_array)
+    
+    # print(d[0])
+    # print(d[1])
+    # print(d[2])
+
+    #print(d[1:,-1:])
+    
+    #print(d[1:,1:-1])
+    if panda:
+        return (pd.DataFrame(data=d[1:,1:-1],
+                    index=d[1:,0],
+                    columns=d[0,1:-1]),
+                pd.DataFrame(data=d[1:,-1:],
+                    index=d[1:,0],
+                    columns=d[0,-1:])
+        )
+                
+    
+    return d 
+
+def getAndProcessMultiFinancialDataJSON(stocks, interval="2m", rge="1d", panda=True, look_fwd=1, simple=True):
     X_total = None
     y_total = None
     first = True
     fd = getMultiFinancialDataJSON(stocks, interval, rge)
-    return processMultiData(fd, panda, look_fwd)
+    if simple:
+        return processMultiDataSimple(fd, panda)
+    else:
+        return processMultiData(fd, panda, look_fwd)
+
+
+def getAndProcessMultiFinancialDataJSONSentiment(stocks, sentiment, interval="2m", rge="1d", panda=True, look_fwd=1):
+    X_total = None
+    y_total = None
+    first = True
+    fd = getMultiFinancialDataJSON(stocks, interval, rge)
+    return processMultiDataSentiment(fd,sentiment,panda)
+
 
 
 
