ba21067be5039220f1da5245e9ce543d81754332
agnes-sharan <agnessharan@gmail.com>
2021-05-15 03:23:10 -0400

    Add code for sentiment inference

---
+++ extractsentiment.py
@@ -0,0 +1,468 @@
+from transformers import BertTokenizer, TFBertForSequenceClassification
+from transformers import InputExample, InputFeatures
+from tensorflow import keras
+import tensorflow as tf
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import warnings
+warnings.filterwarnings('ignore')
+import re
+import string
+import pickle
+import requests
+import os
+import json
+from datetime import datetime
+import numpy as np
+
+# Please follow instructions in README to generate your own BEARER TOKEN
+BEARER_TOKEN = 'Please add token here'
+
+def load_save_pretrained(ckpt_path):
+    model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
+    model.save_weights(ckpt_path)
+
+def load_trained(ckpt_path):
+    model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
+    model.load_weights(ckpt_path)
+    return model
+
+def setup_nltk():
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    nltk.download('wordnet')
+    stopword = set(stopwords.words('english'))
+
+    # Remove URLs and retweets
+    urlPattern = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
+    userPattern = '@[^\s]+'
+    # Replace other common symbols and abbreviations
+    abbreviations = {
+        "$" : " dollar ",
+        "€" : " euro ",
+        "4ao" : "for adults only",
+        "a.m" : "before midday",
+        "a3" : "anytime anywhere anyplace",
+        "aamof" : "as a matter of fact",
+        "acct" : "account",
+        "adih" : "another day in hell",
+        "afaic" : "as far as i am concerned",
+        "afaict" : "as far as i can tell",
+        "afaik" : "as far as i know",
+        "afair" : "as far as i remember",
+        "afk" : "away from keyboard",
+        "app" : "application",
+        "approx" : "approximately",
+        "apps" : "applications",
+        "asap" : "as soon as possible",
+        "atk" : "at the keyboard",
+        "ave." : "avenue",
+        "aymm" : "are you my mother",
+        "ayor" : "at your own risk", 
+        "b&b" : "bed and breakfast",
+        "b+b" : "bed and breakfast",
+        "b.c" : "before christ",
+        "b2b" : "business to business",
+        "b2c" : "business to customer",
+        "b4" : "before",
+        "b4n" : "bye for now",
+        "b@u" : "back at you",
+        "bae" : "before anyone else",
+        "bak" : "back at keyboard",
+        "bbbg" : "bye bye be good",
+        "bbc" : "british broadcasting corporation",
+        "bbias" : "be back in a second",
+        "bbl" : "be back later",
+        "bbs" : "be back soon",
+        "be4" : "before",
+        "bfn" : "bye for now",
+        "blvd" : "boulevard",
+        "bout" : "about",
+        "brb" : "be right back",
+        "bros" : "brothers",
+        "brt" : "be right there",
+        "bsaaw" : "big smile and a wink",
+        "btw" : "by the way",
+        "bwl" : "bursting with laughter",
+        "c/o" : "care of",
+        "cet" : "central european time",
+        "cf" : "compare",
+        "cia" : "central intelligence agency",
+        "csl" : "can not stop laughing",
+        "cu" : "see you",
+        "cul8r" : "see you later",
+        "cv" : "curriculum vitae",
+        "cwot" : "complete waste of time",
+        "cya" : "see you",
+        "cyt" : "see you tomorrow",
+        "dae" : "does anyone else",
+        "dbmib" : "do not bother me i am busy",
+        "diy" : "do it yourself",
+        "dm" : "direct message",
+        "dwh" : "during work hours",
+        "e123" : "easy as one two three",
+        "eet" : "eastern european time",
+        "eg" : "example",
+        "embm" : "early morning business meeting",
+        "encl" : "enclosed",
+        "encl." : "enclosed",
+        "etc" : "and so on",
+        "faq" : "frequently asked questions",
+        "fawc" : "for anyone who cares",
+        "fb" : "facebook",
+        "fc" : "fingers crossed",
+        "fig" : "figure",
+        "fimh" : "forever in my heart", 
+        "ft." : "feet",
+        "ft" : "featuring",
+        "ftl" : "for the loss",
+        "ftw" : "for the win",
+        "fwiw" : "for what it is worth",
+        "fyi" : "for your information",
+        "g9" : "genius",
+        "gahoy" : "get a hold of yourself",
+        "gal" : "get a life",
+        "gcse" : "general certificate of secondary education",
+        "gfn" : "gone for now",
+        "gg" : "good game",
+        "gl" : "good luck",
+        "glhf" : "good luck have fun",
+        "gmt" : "greenwich mean time",
+        "gmta" : "great minds think alike",
+        "gn" : "good night",
+        "g.o.a.t" : "greatest of all time",
+        "goat" : "greatest of all time",
+        "goi" : "get over it",
+        "gps" : "global positioning system",
+        "gr8" : "great",
+        "gratz" : "congratulations",
+        "gyal" : "girl",
+        "h&c" : "hot and cold",
+        "hp" : "horsepower",
+        "hr" : "hour",
+        "hrh" : "his royal highness",
+        "ht" : "height",
+        "ibrb" : "i will be right back",
+        "ic" : "i see",
+        "icq" : "i seek you",
+        "icymi" : "in case you missed it",
+        "idc" : "i do not care",
+        "idk" : "i do not know",
+        "ie" : "that is",
+        "i.e" : "that is",
+        "ifyp" : "i feel your pain",
+        "IG" : "instagram",
+        "iirc" : "if i remember correctly",
+        "ilu" : "i love you",
+        "ily" : "i love you",
+        "imho" : "in my humble opinion",
+        "imo" : "in my opinion",
+        "imu" : "i miss you",
+        "iow" : "in other words",
+        "irl" : "in real life",
+        "j4f" : "just for fun",
+        "jic" : "just in case",
+        "jk" : "just kidding",
+        "jsyk" : "just so you know",
+        "l8r" : "later",
+        "lb" : "pound",
+        "lbs" : "pounds",
+        "ldr" : "long distance relationship",
+        "lmao" : "laugh my ass off",
+        "lol" : "laughing out loud",
+        "ltd" : "limited",
+        "ltns" : "long time no see",
+        "m8" : "mate",
+        "mf" : "motherfucker",
+        "mfs" : "motherfuckers",
+        "mfw" : "my face when",
+        "mofo" : "motherfucker",
+        "mph" : "miles per hour",
+        "mr" : "mister",
+        "mrw" : "my reaction when",
+        "ms" : "miss",
+        "mte" : "my thoughts exactly",
+        "nagi" : "not a good idea",
+        "nbc" : "national broadcasting company",
+        "nbd" : "not big deal",
+        "nfs" : "not for sale",
+        "ngl" : "not going to lie",
+        "nhs" : "national health service",
+        "nrn" : "no reply necessary",
+        "nsfl" : "not safe for life",
+        "nsfw" : "not safe for work",
+        "nth" : "nice to have",
+        "nvr" : "never",
+        "nyc" : "new york city",
+        "oc" : "original content",
+        "og" : "original",
+        "ohp" : "overhead projector",
+        "oic" : "oh i see",
+        "omdb" : "over my dead body",
+        "omg" : "oh my god",
+        "omw" : "on my way",
+        "p.a" : "per annum",
+        "p.m" : "after midday",
+        "pm" : "prime minister",
+        "poc" : "people of color",
+        "pov" : "point of view",
+        "pp" : "pages",
+        "ppl" : "people",
+        "prw" : "parents are watching",
+        "ps" : "postscript",
+        "pt" : "point",
+        "ptb" : "please text back",
+        "pto" : "please turn over",
+        "qpsa" : "what happens", 
+        "ratchet" : "rude",
+        "rbtl" : "read between the lines",
+        "rlrt" : "real life retweet", 
+        "rofl" : "rolling on the floor laughing",
+        "roflol" : "rolling on the floor laughing out loud",
+        "rotflmao" : "rolling on the floor laughing my ass off",
+        "rt" : "retweet",
+        "ruok" : "are you ok",
+        "sfw" : "safe for work",
+        "sk8" : "skate",
+        "smh" : "shake my head",
+        "sq" : "square",
+        "srsly" : "seriously", 
+        "ssdd" : "same stuff different day",
+        "tbh" : "to be honest",
+        "tbs" : "tablespooful",
+        "tbsp" : "tablespooful",
+        "tfw" : "that feeling when",
+        "thks" : "thank you",
+        "tho" : "though",
+        "thx" : "thank you",
+        "tia" : "thanks in advance",
+        "til" : "today i learned",
+        "tl;dr" : "too long i did not read",
+        "tldr" : "too long i did not read",
+        "tmb" : "tweet me back",
+        "tntl" : "trying not to laugh",
+        "ttyl" : "talk to you later",
+        "u" : "you",
+        "u2" : "you too",
+        "u4e" : "yours for ever",
+        "utc" : "coordinated universal time",
+        "w/" : "with",
+        "w/o" : "without",
+        "w8" : "wait",
+        "wassup" : "what is up",
+        "wb" : "welcome back",
+        "wtg" : "way to go",
+        "wtpa" : "where the party at",
+        "wuf" : "where are you from",
+        "wuzup" : "what is up",
+        "wywh" : "wish you were here",
+        "yd" : "yard",
+        "ygtr" : "you got that right",
+        "ynk" : "you never know",
+        "zzz" : "sleeping bored and tired"
+    }
+    return stopword, urlPattern, userPattern, abbreviations
+
+def preprocess_tweets(tweet, stopword, urlPattern, userPattern):
+
+    # Process some commonly seen abbreviations 
+    # and unicode encoded characters to forms
+    # better for the transformer model
+
+    tweet = re.sub(r"he's", "he is", tweet)
+    tweet = re.sub(r"there's", "there is", tweet)
+    tweet = re.sub(r"We're", "We are", tweet)
+    tweet = re.sub(r"That's", "That is", tweet)
+    tweet = re.sub(r"won't", "will not", tweet)
+    tweet = re.sub(r"they're", "they are", tweet)
+    tweet = re.sub(r"Can't", "Cannot", tweet)
+    tweet = re.sub(r"wasn't", "was not", tweet)
+    tweet = re.sub(r"don\x89Ûªt", "do not", tweet)
+    tweet = re.sub(r"aren't", "are not", tweet)
+    tweet = re.sub(r"isn't", "is not", tweet)
+    tweet = re.sub(r"What's", "What is", tweet)
+    tweet = re.sub(r"haven't", "have not", tweet)
+    tweet = re.sub(r"hasn't", "has not", tweet)
+    tweet = re.sub(r"There's", "There is", tweet)
+    tweet = re.sub(r"He's", "He is", tweet)
+    tweet = re.sub(r"It's", "It is", tweet)
+    tweet = re.sub(r"You're", "You are", tweet)
+    tweet = re.sub(r"I'M", "I am", tweet)
+    tweet = re.sub(r"shouldn't", "should not", tweet)
+    tweet = re.sub(r"wouldn't", "would not", tweet)
+    tweet = re.sub(r"i'm", "I am", tweet)
+    tweet = re.sub(r"I\x89Ûªm", "I am", tweet)
+    tweet = re.sub(r"I'm", "I am", tweet)
+    tweet = re.sub(r"Isn't", "is not", tweet)
+    tweet = re.sub(r"Here's", "Here is", tweet)
+    tweet = re.sub(r"you've", "you have", tweet)
+    tweet = re.sub(r"you\x89Ûªve", "you have", tweet)
+    tweet = re.sub(r"we're", "we are", tweet)
+    tweet = re.sub(r"what's", "what is", tweet)
+    tweet = re.sub(r"couldn't", "could not", tweet)
+    tweet = re.sub(r"we've", "we have", tweet)
+    tweet = re.sub(r"it\x89Ûªs", "it is", tweet)
+    tweet = re.sub(r"doesn\x89Ûªt", "does not", tweet)
+    tweet = re.sub(r"It\x89Ûªs", "It is", tweet)
+    tweet = re.sub(r"Here\x89Ûªs", "Here is", tweet)
+    tweet = re.sub(r"who's", "who is", tweet)
+    tweet = re.sub(r"I\x89Ûªve", "I have", tweet)
+    tweet = re.sub(r"y'all", "you all", tweet)
+    tweet = re.sub(r"can\x89Ûªt", "cannot", tweet)
+    tweet = re.sub(r"would've", "would have", tweet)
+    tweet = re.sub(r"it'll", "it will", tweet)
+    tweet = re.sub(r"we'll", "we will", tweet)
+    tweet = re.sub(r"wouldn\x89Ûªt", "would not", tweet)
+    tweet = re.sub(r"We've", "We have", tweet)
+    tweet = re.sub(r"he'll", "he will", tweet)
+    tweet = re.sub(r"Y'all", "You all", tweet)
+    tweet = re.sub(r"Weren't", "Were not", tweet)
+    tweet = re.sub(r"Didn't", "Did not", tweet)
+    tweet = re.sub(r"they'll", "they will", tweet)
+    tweet = re.sub(r"they'd", "they would", tweet)
+    tweet = re.sub(r"DON'T", "DO NOT", tweet)
+    tweet = re.sub(r"That\x89Ûªs", "That is", tweet)
+    tweet = re.sub(r"they've", "they have", tweet)
+    tweet = re.sub(r"i'd", "I would", tweet)
+    tweet = re.sub(r"should've", "should have", tweet)
+    tweet = re.sub(r"You\x89Ûªre", "You are", tweet)
+    tweet = re.sub(r"where's", "where is", tweet)
+    tweet = re.sub(r"Don\x89Ûªt", "Do not", tweet)
+    tweet = re.sub(r"we'd", "we would", tweet)
+    tweet = re.sub(r"i'll", "I will", tweet)
+    tweet = re.sub(r"weren't", "were not", tweet)
+    tweet = re.sub(r"They're", "They are", tweet)
+    tweet = re.sub(r"Can\x89Ûªt", "Cannot", tweet)
+    tweet = re.sub(r"you\x89Ûªll", "you will", tweet)
+    tweet = re.sub(r"I\x89Ûªd", "I would", tweet)
+    tweet = re.sub(r"let's", "let us", tweet)
+    tweet = re.sub(r"it's", "it is", tweet)
+    tweet = re.sub(r"can't", "cannot", tweet)
+    tweet = re.sub(r"don't", "do not", tweet)
+    tweet = re.sub(r"you're", "you are", tweet)
+    tweet = re.sub(r"i've", "I have", tweet)
+    tweet = re.sub(r"that's", "that is", tweet)
+    tweet = re.sub(r"i'll", "I will", tweet)
+    tweet = re.sub(r"doesn't", "does not", tweet)
+    tweet = re.sub(r"i'd", "I would", tweet)
+    tweet = re.sub(r"didn't", "did not", tweet)
+    tweet = re.sub(r"ain't", "am not", tweet)
+    tweet = re.sub(r"you'll", "you will", tweet)
+    tweet = re.sub(r"I've", "I have", tweet)
+    tweet = re.sub(r"Don't", "do not", tweet)
+    tweet = re.sub(r"I'll", "I will", tweet)
+    tweet = re.sub(r"I'd", "I would", tweet)
+    tweet = re.sub(r"Let's", "Let us", tweet)
+    tweet = re.sub(r"you'd", "You would", tweet)
+    tweet = re.sub(r"It's", "It is", tweet)
+    tweet = re.sub(r"Ain't", "am not", tweet)
+    tweet = re.sub(r"Haven't", "Have not", tweet)
+    tweet = re.sub(r"Could've", "Could have", tweet)
+    tweet = re.sub(r"youve", "you have", tweet)  
+    tweet = re.sub(r"donå«t", "do not", tweet)  
+    tweet = re.sub(r"some1", "someone", tweet)
+    tweet = re.sub(r"yrs", "years", tweet)
+    tweet = re.sub(r"hrs", "hours", tweet)
+    tweet = re.sub(r"2morow|2moro", "tomorrow", tweet)
+    tweet = re.sub(r"2day", "today", tweet)
+    tweet = re.sub(r"4got|4gotten", "forget", tweet)
+    tweet = re.sub(r"b-day|bday", "b-day", tweet)
+    tweet = re.sub(r"mother's", "mother", tweet)
+    tweet = re.sub(r"mom's", "mom", tweet)
+    tweet = re.sub(r"dad's", "dad", tweet)
+    tweet = re.sub(r"hahah|hahaha|hahahaha", "haha", tweet)
+    tweet = re.sub(r"lmao|lolz|rofl", "lol", tweet)
+    tweet = re.sub(r"thanx|thnx", "thanks", tweet)
+    tweet = re.sub(r"goood", "good", tweet)
+    tweet = re.sub(r"some1", "someone", tweet)
+    tweet = re.sub(r"some1", "someone", tweet)
+    tweet = tweet.lower()
+    tweet=tweet[1:]
+
+    # Removing all URls 
+    tweet = re.sub(urlPattern,'',tweet)
+    # Removing all @username.
+    tweet = re.sub(userPattern,'', tweet)
+    #Remove punctuations
+    tweet = tweet.translate(str.maketrans("","",string.punctuation))
+    #tokenizing words
+    tokens = word_tokenize(tweet)
+    #Removing Stop Words
+    final_tokens = [w for w in tokens if w not in stopword]
+
+    #reducing a word to its word stem 
+    wordLemm = WordNetLemmatizer()
+    finalwords=[]
+    for w in final_tokens:
+      if len(w)>1:
+        word = wordLemm.lemmatize(w)
+        finalwords.append(word)
+
+    return ' '.join(finalwords)
+
+# Function call to apply abbreviation conversion on text
+def convert_abbrev_in_text(tweet, abbreviations):
+    t=[]
+    words=tweet.split()
+    t = [abbreviations[w.lower()] if w.lower() in abbreviations.keys() else w for w in words]
+    return ' '.join(t)  
+
+def auth():
+    return BEARER_TOKEN
+
+def create_url(symbol, timestamp):
+    query = symbol
+    ts = datetime.utcfromtimestamp(timestamp)
+
+    until_date = "-".join([str(ts.year), str(ts.month), str(ts.day)])
+    print(until_date)
+    url = "https://api.twitter.com/1.1/search/tweets.json?q={}&until={}&count=30&result_type=recent&lang=en".format(
+        query, until_date
+    )
+    return url
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+def connect_to_endpoint(url, headers):
+    response = requests.request("GET", url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+def get_sentiment(unix_timestamps=["1620504200"], ticker="TSLA"):
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    model = load_trained('bert')
+    stopword, urlPattern, userPattern, abbreviations = setup_nltk()
+    bearer_token = auth()
+    sentiments = []
+    for timestamp in unix_timestamps:
+        timestamp = int(timestamp)
+        url = create_url(ticker, timestamp)
+        headers = create_headers(bearer_token)
+        json_response = connect_to_endpoint(url, headers)
+        response = json.dumps(json_response, indent=4, sort_keys=True)
+        response = json.loads(response)
+        tweets = []
+        ts = datetime.utcfromtimestamp(timestamp)
+        for resp in response["statuses"]:
+            time_obj = datetime.strptime(resp["created_at"], "%a %b %d %H:%M:%S +0000 %Y")
+            if time_obj.day < ts.day or time_obj.hour < ts.hour or time_obj.minute < ts.minute or time_obj.second < ts.second:
+                tweets.append(resp["text"])
+        local_sentiments = []
+        tweets = list(map(lambda x: preprocess_tweets(x, stopword, urlPattern, userPattern), tweets))
+        tweets = list(map(lambda x: convert_abbrev_in_text(x, abbreviations), tweets))
+        tf_batch = tokenizer(tweets, max_length=64, padding=True, truncation=True, return_tensors='tf')
+        tf_outputs = model(tf_batch)
+        tf_predictions = tf.nn.softmax(tf_outputs[0], axis=-1)
+        labels = [0,1]
+        label = tf.argmax(tf_predictions, axis=1)
+        label = label.numpy()
+        if(np.count_nonzero(label == 1) >= 15):
+            sentiments.append(1)
+        else:
+            sentiments.append(1)
+    return sentiments
